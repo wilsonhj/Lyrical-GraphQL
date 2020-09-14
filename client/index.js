e54580b65f0eec82c6d74049afd26b31cb776e96
James Yamagata <wilsonhj@uci.edu>
2020-09-13 19:04:00 -0700

    Converting project @apollo/client v3.0. Successfuly connected to MongoDB after hours of debugging.

--- client/index.js
+++ client/index.js
@@ -1,8 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { ApolloClient, InMemoryCache, gql, useQuery, createHttpLink, ApolloProvider } from '@apollo/client';
+
+const cache = new InMemoryCache();
+const link = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
+
+const client = new ApolloClient({
+  link: link,
+  cache: cache
+});
+
+// client
+//   .query({
+//     query: gql`
+//       query GetRates {
+//         rates(current: "USD"){
+//           currency
+//         }
+//       }
+//     `
+//   })
+//   .then(res => console.log(res));
+
+// const EXCHANGE_RATES = gql`
+//   query GetExchangeRates{
+//     rates(currency: "USD"){
+//       currency
+//       rate
+//     }
+//   }
+//   `;
+
+// function ExchangeRates() {
+//   const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+//   if (loading) return <p>Loading...</p>;
+//   if (error) return <p>Error :(</p>;
+
+//   return data.rates.map(({ currency, rate }) => (
+//     <div key={currency}>
+//       <p>
+//         {currency}: {rate}
+//       </p>
+//     </div>
+//   ));
+// };
+
+
+
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <div>Lyrical</div>
+    </ApolloProvider>
+  );
 };
 
 ReactDOM.render(
--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const models = require('./models');
-const expressGraphQL = require('express-graphql');
+// const expressGraphQL = require('express-graphql'); // deprecated
+const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
 
 const app = express();
 
+// private key = '0c3e79a7-1a01-4cb7-ac02-1c9d89458d9d'
+
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://hjw:glxIPvUkTyytKjlJ@lyrical-cluster.qolbq.mongodb.net/lyrical-cluster?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
@@ -20,10 +23,16 @@ mongoose.connection
     .on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
+app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
-}));
+  })
+);
+
+// app.use('/graphql', expressGraphQL({ // deprecated
+//   schema,
+//   graphiql: true
+// }));
 
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
